2f2fce7b91079caa5ebbd6991ff2824ed0579bfb
nsbn65 <saito12pbyf8o@gmail.com>
2022-11-09 10:42:01 +0900

    todo-pj

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Request\PostRequest;
 
@@ -9,48 +10,62 @@ class PostController extends Controller
 {
     public function index()
     {
-    $posts = Post::all();
-    return view('index', ['posts' => $posts]);
+        $posts = Post::all();
+        return view('index', ['posts' => $posts]);
     }
 
     public function find()
     {
-    return view('find', ['input' => '']);
+        return view('find', ['input' => '']);
     }
 
     public function search(Request $request)
     {
-    $post = Post::find($request->input);
-    $param = [
-        'post' => $post,
-        'input' => $request->input
-    ];
-    return view('find', $param);
+        $post = Post::find($request->input);
+        $param = [
+            'post' => $post,
+            'input' => $request->input
+        ];
+        return view('find', $param);
+    }
+
+    public function add()
+    {
+    return view('add');
+    }
+
+    public function create(PostRequest $request)
+    {
+        $this->validate($request, Post::$rules);
+        $form = $request->all();
+        Post::create($form);
+        unset($form['_token']);
+        return redirect('/');
     }
 
     public function edit(Request $request)
     {
-    $post = Post::find($request->id);
-    return view('edit', ['form' => $post]);
+        $post = Post::find($request->id);
+        return view('edit', ['form' => $post]);
     }
 
     public function update(PostRequest $request)
     {
-    $form = $request->all();
-    unset($form['_token']);
-    Post::where('id', $request->id)->update($form);
-    return redirect('/');
+        $form = $request->all();
+        unset($form['_token']);
+        Post::where('id', $request->id)->update($form);
+        return redirect('/');
     }
 
     public function delete(Request $request)
     {
-    $post = Post::find($request->id);
-    return view('delete', ['form' => $post]);
+        $post = Post::find($request->id);
+        return view('delete', ['form' => $post]);
     }
 
     public function remove(Request $request)
     {
-    Post::find($request->id)->delete();
-    return redirect('/');
+        Post::find($request->id)->delete();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use App\Http\Controllers\PostController;
 
 
 Route::get('/',[PostController::class,'index']);//一覧表示
-/*
-Route::post('/create',[PostController::class,'create']);//タスク追加
-Route::post('/update',[PostController::class,'update']);//タスク更新
-Route::delete('/delete',[PostController::class,'delete']);//タスク削除
-*/
 Route::get('/find', [PostController::class, 'find']);
 Route::post('/find', [PostController::class, 'search']);
 Route::get('/add', [PostController::class, 'add']);
@@ -18,3 +13,8 @@ Route::get('/edit', [PostController::class, 'edit']);
 Route::post('/edit', [PostController::class, 'update']);
 Route::get('/delete', [AuthorController::class, 'delete']);
 Route::post('/delete', [AuthorController::class, 'remove']);
+/*
+Route::post('/create',[PostController::class,'create']);//タスク追加
+Route::post('/update',[PostController::class,'update']);//タスク更新
+Route::delete('/delete',[PostController::class,'delete']);//タスク削除
+*/
